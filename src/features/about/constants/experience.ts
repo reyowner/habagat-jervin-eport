898e70a9d7bd7eb1befdab75b9f427b16f6d52bb
reyowner <domasigreoner@gmail.com>
2026-08-18 19:18:27 +0800

    update experience

--- src/features/about/constants/experience.ts
+++ src/features/about/constants/experience.ts
@@ -8,41 +8,41 @@ export const EXPERIENCE: Experience[] = [
   {
     year: "2021 — Present",
     role: "Freelance Architect",
-    place: "HACARA+ Architects – Bataan, PH",
+    place: "HACARA+ Architects  -  Bataan, PH",
   },
   {
     year: "April 2025 — June 2026",
     role: "Architectural Designer",
-    place: "Raynor Redeveloped LLC – Portland OR",
+    place: "Raynor Redeveloped LLC  -  Portland OR",
   },
   {
     year: "July 2024 — February 2025",
     role: "Architect | Creative Designer",
-    place: "Eichholtz Long Island – New York",
+    place: "Eichholtz Long Island  -  New York",
   },
   {
     year: "August 2024 — February 2026",
     role: "Architectural Instructor",
-    place: "Bataan Heroes College – Bataan, PH",
+    place: "Bataan Heroes College  -  Bataan, PH",
   },
   {
-    year: "2019 — 2022",
+    year: "November 2019 — May 2025",
     role: "Architectural Designer & Illustrator",
-    place: "Upwork, Fiver – Remote Work",
+    place: "Upwork, Fiver  -  Remote Work",
   },
   {
     year: "June 2020 — December 2021",
     role: "Content Producer & Stream Engineer",
-    place: "When in Manila + Webfunc.site – Remote Work",
+    place: "When in Manila + Webfunc.site  -  Remote Work",
   },
   {
     year: "September 2018 — June 2019",
     role: "Architecture Intern / Project Coordinator",
-    place: "Peralta Burgos Design and Construction – Bataan, PH",
+    place: "Peralta Burgos Design and Construction  -  Bataan, PH",
   },
   {
     year: "January 2015 — October 2021",
     role: "Graphic Designer & Illustrator",
-    place: "JSJ Digital Printing – Bataan, PH",
+    place: "JSJ Digital Printing  -  Bataan, PH",
   },
 ];
